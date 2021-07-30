0bcbde5dbb30398e91643a5b85166f280d553d9e
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2021-07-30 14:14:59 -0400

    Make Percent a comparable

--- src/main/kotlin/xyz/qwewqa/relivesim/stage/Percent.kt
+++ src/main/kotlin/xyz/qwewqa/relivesim/stage/Percent.kt
@@ -4,7 +4,7 @@ package xyz.qwewqa.relivesim.stage
  * Value class representing a percentage value.
  */
 @JvmInline
-value class Percent(val value: Double) {
+value class Percent(val value: Double) : Comparable<Percent> {
     operator fun times(other: Percent) = Percent(this.asDouble() * other.value)
     operator fun div(other: Percent) = Percent(this.value / other.asDouble())
     operator fun plus(other: Percent) = Percent(this.value + other.value)
@@ -12,6 +12,8 @@ value class Percent(val value: Double) {
 
     operator fun unaryPlus() = this
     operator fun unaryMinus() = Percent(-this.value)
+
+    override operator fun compareTo(other: Percent) = value.compareTo(other.value)
 }
 
 inline val Int.percent get() = Percent(this.toDouble())
@@ -25,15 +27,3 @@ operator fun Double.times(percent: Percent) = this * percent.asDouble()
 
 operator fun Percent.times(int: Int) = this.asDouble() * int
 operator fun Percent.times(double: Double) = this.asDouble() * double
-
-fun Percent.coerceAtMost(maximumValue: Percent): Percent {
-    return Percent(value.coerceAtMost(maximumValue.value))
-}
-
-fun Percent.coerceAtLeast(minimumValue: Percent): Percent {
-    return Percent(value.coerceAtLeast(minimumValue.value))
-}
-
-fun Percent.coerceIn(minimumValue: Percent, maximumValue: Percent): Percent {
-    return Percent(value.coerceIn(minimumValue.value, maximumValue.value))
-}
