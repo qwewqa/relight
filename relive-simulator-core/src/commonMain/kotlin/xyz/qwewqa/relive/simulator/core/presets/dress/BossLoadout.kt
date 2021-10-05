c4626b5b3fd5f0c785c068573c2fe80dc85fbc45
Savivo <n.savivo@gmail.com>
2021-10-05 18:12:44 -0400

    Add Seisho version of Boss 2

--- relive-simulator-core/src/commonMain/kotlin/xyz/qwewqa/relive/simulator/core/presets/dress/BossLoadout.kt
+++ relive-simulator-core/src/commonMain/kotlin/xyz/qwewqa/relive/simulator/core/presets/dress/BossLoadout.kt
@@ -1,10 +1,7 @@
 package xyz.qwewqa.relive.simulator.core.presets.dress
 
 import xyz.qwewqa.relive.simulator.core.presets.dress.boss.tr10.*
-import xyz.qwewqa.relive.simulator.core.presets.dress.boss.tr12.tr12Boss2
-import xyz.qwewqa.relive.simulator.core.presets.dress.boss.tr12.tr12Boss2Strategy
-import xyz.qwewqa.relive.simulator.core.presets.dress.boss.tr12.tr12TowerHikari
-import xyz.qwewqa.relive.simulator.core.presets.dress.boss.tr12.tr12TowerHikariStrategy
+import xyz.qwewqa.relive.simulator.core.presets.dress.boss.tr12.*
 import xyz.qwewqa.relive.simulator.core.presets.dress.boss.tr9.tr9FaithMisora
 import xyz.qwewqa.relive.simulator.core.presets.dress.boss.tr9.tr9FaithMisoraStrategy
 import xyz.qwewqa.relive.simulator.core.stage.loadout.ActorLoadout
@@ -18,5 +15,6 @@ val bossLoadouts = listOf(
     BossLoadout(tr10HellsingMichiru, tr10HellsingMichiruStrategy),
     BossLoadout(tr10FaithMisora, tr10FaithMisoraStrategy),
     BossLoadout(tr12Boss2, tr12Boss2Strategy),
-    BossLoadout(tr12TowerHikari, tr12TowerHikariStrategy)
+    BossLoadout(tr12Boss2Seisho, tr12Boss2SeishoStrategy),
+    BossLoadout(tr12TowerHikari, tr12TowerHikariStrategy),
 ).associateBy { it.loadout.name }
