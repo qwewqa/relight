ecd892dab5df229838ca28dabbe4ac53f8564b22
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2021-10-15 21:07:43 -0700

    Add event bonus passive

--- relive-simulator-core/src/commonMain/kotlin/xyz/qwewqa/relive/simulator/core/stage/passive/BossPassive.kt
+++ relive-simulator-core/src/commonMain/kotlin/xyz/qwewqa/relive/simulator/core/stage/passive/BossPassive.kt
@@ -2,12 +2,13 @@ package xyz.qwewqa.relive.simulator.core.stage.passive
 
 import xyz.qwewqa.relive.simulator.core.stage.ActionContext
 import xyz.qwewqa.relive.simulator.core.stage.actor.Attribute
-import xyz.qwewqa.relive.simulator.core.stage.actor.advantagedAgainst
 import xyz.qwewqa.relive.simulator.core.stage.actor.disadvantagedAgainst
 import xyz.qwewqa.relive.simulator.core.stage.autoskill.PassiveEffect
 import xyz.qwewqa.relive.simulator.core.stage.autoskill.PassiveEffectCategory
 import xyz.qwewqa.relive.simulator.core.stage.buff.abnormalBuffs
 import xyz.qwewqa.relive.simulator.core.stage.condition.Condition
+import xyz.qwewqa.relive.simulator.core.stage.dress.Dress
+import xyz.qwewqa.relive.simulator.core.stage.log
 
 object AbnormalGuardPassive : PassiveEffect {
     override val category = PassiveEffectCategory.TurnStartPositive
@@ -41,5 +42,26 @@ object BossElementResistPassive : PassiveEffect {
         self.againstAttributeDamageTakenDown[disadvantagedAgainst] = 0
         self.againstAttributeDamageDealtUp[Attribute.Dream] = 0
         self.againstAttributeDamageTakenDown[Attribute.Dream] = 0
+    }
+}
+
+/**
+ * A passive which sets the event bonuses against the boss which has this passive.
+ */
+class EventBonusPassive(val values: Map<Dress, Int>) : PassiveEffect {
+    override val category = PassiveEffectCategory.TurnStartPositive
+
+    override fun activate(context: ActionContext, value: Int, time: Int, condition: Condition) = context.run {
+        enemy.actors.values.forEach { actor ->
+            val bonus = values[actor.dress.base ?: actor.dress]
+            if (bonus != null) {
+                actor.context.run {
+                    team.actors.values.forEach { member ->
+                        member.eventBonus += bonus
+                    }
+                    log("Event Bonus") { "Team event bonus $bonus." }
+                }
+            }
+        }
     }
 }
