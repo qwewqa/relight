3278af380824c1740ed437b896fdd19063bfb528
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2021-11-14 16:57:29 -0800

    Add sakura hikari to the actors list

--- relive-simulator-core/src/commonMain/kotlin/xyz/qwewqa/relive/simulator/core/presets/dress/PlayerDress.kt
+++ relive-simulator-core/src/commonMain/kotlin/xyz/qwewqa/relive/simulator/core/presets/dress/PlayerDress.kt
@@ -13,6 +13,7 @@ import xyz.qwewqa.relive.simulator.core.presets.dress.back.space.JusticeNana
 import xyz.qwewqa.relive.simulator.core.presets.dress.back.space.SanadaMahiru
 import xyz.qwewqa.relive.simulator.core.presets.dress.back.wind.FoolYachiyo
 import xyz.qwewqa.relive.simulator.core.presets.dress.back.wind.ImperialCombatRevueFlowerDivisionSakura
+import xyz.qwewqa.relive.simulator.core.presets.dress.back.wind.SakuraShingujiHikari
 import xyz.qwewqa.relive.simulator.core.presets.dress.middle.cloud.MitsuhideKaren
 import xyz.qwewqa.relive.simulator.core.presets.dress.middle.cloud.StageGirlMisora
 import xyz.qwewqa.relive.simulator.core.presets.dress.middle.cloud.StageGirlNana
@@ -39,6 +40,7 @@ val playerDresses = listOf(
     MitsuhideKaren,
     NewYorkCombatRevueGemie,
     ParisCombatRevueFlowerDivisionErika,
+    SakuraShingujiHikari,
     SanadaMahiru,
     StageGirlClaudine,
     StageGirlHikari,
