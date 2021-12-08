e4d2d2af8f330e7952e862140ff4a191b054bdea
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2021-12-08 15:40:53 -0800

    Correct typo

--- relive-simulator-core/src/commonMain/kotlin/xyz/qwewqa/relive/simulator/core/presets/condition/DressCategoryCondition.kt
+++ relive-simulator-core/src/commonMain/kotlin/xyz/qwewqa/relive/simulator/core/presets/condition/DressCategoryCondition.kt
@@ -3,10 +3,12 @@ package xyz.qwewqa.relive.simulator.core.presets.condition
 import xyz.qwewqa.relive.simulator.core.stage.condition.NamedCondition
 import xyz.qwewqa.relive.simulator.core.stage.dress.DressCategory
 
+// Note: these aren't being added to the condition list for now, since they're only being used internally
+
 val ArcanaOnlyCondition = NamedCondition("Arcana") {
     it.dress.category == DressCategory.Arcana
 }
 
-val StageGirlOnlyCondition = NamedCondition("Arcana") {
+val StageGirlOnlyCondition = NamedCondition("Stage Girl") {
     it.dress.category == DressCategory.StageGirl
 }
