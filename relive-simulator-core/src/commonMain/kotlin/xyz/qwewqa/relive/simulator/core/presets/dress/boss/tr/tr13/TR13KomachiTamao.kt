9676eb8aea11595cec480cc219db55130adecaa5
Savivo <n.savivo@gmail.com>
2021-12-17 08:38:00 -0500

    WW's December TR bosses (labeled as TR13 because we got JP's 13 for our 12)

--- relive-simulator-core/src/commonMain/kotlin/xyz/qwewqa/relive/simulator/core/presets/dress/boss/tr/tr13/TR13KomachiTamao.kt
+++ relive-simulator-core/src/commonMain/kotlin/xyz/qwewqa/relive/simulator/core/presets/dress/boss/tr/tr13/TR13KomachiTamao.kt
@@ -1,6 +1,6 @@
 package xyz.qwewqa.relive.simulator.core.presets.dress.boss.tr.tr13
 
-import xyz.qwewqa.relive.simulator.core.presets.dress.middle.snow.HopeLalafin
+import xyz.qwewqa.relive.simulator.core.presets.dress.back.cloud.JudgementShizuha
 import xyz.qwewqa.relive.simulator.core.presets.dress.boss.tr.trEventBonusPassive
 import xyz.qwewqa.relive.simulator.stage.character.Character
 import xyz.qwewqa.relive.simulator.stage.character.DamageType
@@ -12,6 +12,172 @@ import xyz.qwewqa.relive.simulator.core.stage.loadout.ActorLoadout
 import xyz.qwewqa.relive.simulator.core.stage.dress.Dress
 import xyz.qwewqa.relive.simulator.core.stage.passive.AbnormalGuardPassive
 import xyz.qwewqa.relive.simulator.core.stage.strategy.FixedStrategy
+
+val tr13KomachiTamao = ActorLoadout(
+    "TR13 Komachi Tamao",
+    Dress(
+        name = "Komachi Tamao",
+        character = Character.Tamao,
+        attribute = Attribute.Dream,
+        damageType = DamageType.Special,
+        position = Position.None,
+        stats = defaultDressStats.copy(
+            hp = 4_500_000,
+            actPower = 2300,
+            normalDefense = 650,
+            specialDefense = 650,
+            agility = 1,
+        ),
+        acts = actsOf(
+            ActType.Act1("Slash", 2) {
+                targetBack().act {
+                    attack(
+                        modifier = 100,
+                        hitCount = 1,
+                    )
+                }
+            },
+            ActType.Act2("Strong Slash", 2) {
+                targetBack().act {
+                    attack(
+                        modifier = 150,
+                        hitCount = 1,
+                    )
+                }
+            },
+            ActType.Act3("Triple Slash", 2) {
+                targetBack(3).act {
+                    attack(
+                        modifier = 70,
+                        hitCount = 3,
+                    )
+                }
+            },
+            ActType.Act4("Strong Triple Slash", 2) {
+                targetBack(3).act {
+                    attack(
+                        modifier = 100,
+                        hitCount = 3,
+                    )
+                }
+            },
+            ActType.Act5("Counter Concerto", 2) {
+                targetAoe().act {
+                    attack(
+                        modifier = 120,
+                        hitCount = 2,
+                    )
+                }
+            },
+            ActType.Act6("Pursuit Concerto", 2) {
+                targetAoe().act {
+                    attack(
+                        modifier = 150,
+                        hitCount = 4,
+                    )
+                }
+            },
+            ActType.Act7("Helpless Concerto", 2) {
+                targetAoe().act {
+                    applyBuff(
+                        effect = DexterityDownBuff,
+                        value = 50,
+                        turns = 3,
+                    )
+                    applyBuff(
+                        effect = CriticalDownBuff,
+                        value = 50,
+                        turns = 3,
+                    )
+                }
+            },
+            ActType.Act8("Evasion Dance", 2) {
+                targetSelf().act {
+                    applyBuff(
+                        effect = EvasionRateUpBuff,
+                        value = 50,
+                        turns = 2,
+                    )
+                }
+            },
+            ActType.Act9("Perfect Aim Flurry", 2) {
+                targetSelf().act {
+                    applyBuff(
+                        effect = ActPowerUpBuff,
+                        value = 50,
+                        turns = 3,
+                    )
+                    applyBuff(
+                        effect = PerfectAim,
+                        turns = 1,
+                    )
+                }
+                targetAoe().act {
+                    attack(
+                        modifier = 150,
+                        hitCount = 4,
+                    )
+                }
+            },
+            ActType.ClimaxAct("Cherry Blossom Quick-Draw NEO", 2) {
+                targetSelf().act {
+                    applyBuff(
+                        effect = ActPowerUpBuff,
+                        value = 50,
+                        turns = 3,
+                    )
+                    applyBuff(
+                        effect = PerfectAim,
+                        turns = 1,
+                    )
+                }
+                targetAoe().act {
+                    attack(
+                        modifier = 200,
+                        hitCount = 4,
+                    )
+                }
+            },
+            ActType.ConfusionAct("Slash", 2) {
+                targetAllyRandom().act {
+                    attack(
+                        modifier = 105,
+                        hitCount = 1,
+                    )
+                }
+            },
+        ),
+        autoSkills = listOf(
+            trEventBonusPassive(JudgementShizuha),
+            AbnormalGuardPassive.new(),
+        ),
+    ),
+)
+
+val tr13KomachiTamaoStrategy = FixedStrategy {
+    val boss = this.team.actors.values.first()
+
+    when (turn) {
+        1 -> {
+            +boss[ActType.Act8]
+            +boss[ActType.Act1]
+            +boss[ActType.Act3]
+        }
+        2 -> {
+            +boss[ActType.Act7]
+            +boss[ActType.Act4]
+            +boss[ActType.Act5]
+        }
+        3 -> {
+            +boss[ActType.Act8]
+            +boss[ActType.Act5]
+            +boss[ActType.ClimaxAct]
+        }
+        else -> {
+            error("Unsupported.")
+        }
+    }
+}
 
 val tr13KomachiTamaoDiff4 = ActorLoadout(
     "TR13 Komachi Tamao Difficulty 4",
@@ -148,7 +314,7 @@ val tr13KomachiTamaoDiff4 = ActorLoadout(
             },
         ),
         autoSkills = listOf(
-            trEventBonusPassive(HopeLalafin),
+            trEventBonusPassive(JudgementShizuha),
             AbnormalGuardPassive.new(),
         ),
     ),
@@ -157,7 +323,7 @@ val tr13KomachiTamaoDiff4 = ActorLoadout(
 val tr13KomachiTamaoDiff4Strategy = FixedStrategy {
     val boss = this.team.actors.values.first()
 
-    when (turn) { //TODO() Actual moveset
+    when (turn) {
         1 -> {
             +boss[ActType.Act8]
             +boss[ActType.Act1]
@@ -165,7 +331,7 @@ val tr13KomachiTamaoDiff4Strategy = FixedStrategy {
         }
         2 -> {
             +boss[ActType.Act7]
-            +boss[ActType.Act3]
+            +boss[ActType.Act4]
             +boss[ActType.Act5]
         }
         3 -> {
@@ -174,13 +340,19 @@ val tr13KomachiTamaoDiff4Strategy = FixedStrategy {
             +boss[ActType.ClimaxAct]
         }
         4 ->{
-
+            +boss[ActType.Act7]
+            +boss[ActType.Act2]
+            +boss[ActType.Act5]
         }
         5 ->{
-
+            +boss[ActType.Act8]
+            +boss[ActType.Act4]
+            +boss[ActType.Act6]
         }
         6 ->{
-
+            +boss[ActType.Act7]
+            +boss[ActType.Act9]
+            +boss[ActType.Act6]
         }
         else -> error("Not supported.")
     }
--- relive-simulator-core/src/commonMain/kotlin/xyz/qwewqa/relive/simulator/core/presets/dress/boss/tr/tr13/TR13SetsunaIchie.kt
+++ relive-simulator-core/src/commonMain/kotlin/xyz/qwewqa/relive/simulator/core/presets/dress/boss/tr/tr13/TR13SetsunaIchie.kt
@@ -1,6 +1,6 @@
 package xyz.qwewqa.relive.simulator.core.presets.dress.boss.tr.tr13
 
-import xyz.qwewqa.relive.simulator.core.presets.dress.middle.snow.HopeLalafin
+import xyz.qwewqa.relive.simulator.core.presets.dress.back.cloud.JudgementShizuha
 import xyz.qwewqa.relive.simulator.core.presets.dress.boss.tr.trEventBonusPassive
 import xyz.qwewqa.relive.simulator.stage.character.Character
 import xyz.qwewqa.relive.simulator.stage.character.DamageType
@@ -12,6 +12,159 @@ import xyz.qwewqa.relive.simulator.core.stage.loadout.ActorLoadout
 import xyz.qwewqa.relive.simulator.core.stage.dress.Dress
 import xyz.qwewqa.relive.simulator.core.stage.passive.AbnormalGuardPassive
 import xyz.qwewqa.relive.simulator.core.stage.strategy.FixedStrategy
+
+val tr13SetsunaIchie = ActorLoadout(
+    "TR13 Setsuna Ichie",
+    Dress(
+        name = "Setsuna Ichie",
+        character = Character.Ichie,
+        attribute = Attribute.Dream,
+        damageType = DamageType.Normal,
+        position = Position.None,
+        stats = defaultDressStats.copy(
+            hp = 3_000_000,
+            actPower = 2300,
+            normalDefense = 650,
+            specialDefense = 650,
+            agility = 1,
+        ),
+        acts = actsOf(
+            ActType.Act1("Strong Flash", 2) {
+                targetFront().act {
+                    attack(
+                        modifier = 150,
+                        hitCount = 1,
+                    )
+                }
+            },
+            ActType.Act2("Violent Twin Flash", 2) {
+                targetFront(2).act {
+                    attack(
+                        modifier = 120,
+                        hitCount = 2,
+                    )
+                }
+            },
+            ActType.Act3("Triple Flash", 2) {
+                targetFront(3).act {
+                    attack(
+                        modifier = 70,
+                        hitCount = 3,
+                    )
+                }
+            },
+            ActType.Act4("Violent Triple Flash", 2) {
+                targetFront(3).act {
+                    attack(
+                        modifier = 100,
+                        hitCount = 3,
+                    )
+                }
+            },
+            ActType.Act5("Counter Concerto", 2) {
+                targetAoe().act{
+                    attack(
+                        modifier = 120,
+                        hitCount = 2,
+                    )
+                }
+            },
+            ActType.Act6("Weakening Concerto", 2) {
+                targetAoe().act{
+                    applyBuff(
+                        effect = ActPowerDownBuff,
+                        value = 50,
+                        turns = 3,
+                    )
+                }
+            },
+            ActType.Act7("Purifying Dance", 2) {
+                targetSelf().act{
+                    dispelTimed(BuffCategory.Negative)
+                }
+                targetAoe().act{
+                    dispelTimed(BuffCategory.Positive)
+                }
+            },
+            ActType.Act8("Noxious Flower Dance", 2){
+                targetAoe().act{
+                    attack(
+                        modifier = 150,
+                        hitCount = 4,
+                    )
+                    applyBuff(
+                        effect = PoisonBuff,
+                        value = 10000,
+                        turns = 3,
+                    )
+                }
+            },
+            ActType.ClimaxAct("Flower Festival Type 3 NEO", 2) {
+                targetAoe().act{
+                    attack(
+                        modifier = 200,
+                        hitCount = 4,
+                    )
+                    applyBuff(
+                        effect = PoisonBuff,
+                        value = 10000,
+                        turns = 3,
+                    )
+                }
+            },
+            ActType.ConfusionAct("Slash", 2) {
+                targetAllyRandom().act {
+                    attack(
+                        modifier = 105,
+                        hitCount = 1,
+                    )
+                }
+            },
+        ),
+        autoSkills = listOf(
+            trEventBonusPassive(JudgementShizuha),
+            AbnormalGuardPassive.new(),
+        ),
+    ),
+)
+
+val tr13SetsunaIchieStrategy = FixedStrategy {
+    val boss = this.team.actors.values.first()
+
+    when (turn) {
+        1 -> {
+            +boss[ActType.Act6]
+            +boss[ActType.Act1]
+            +boss[ActType.Act3]
+        }
+        2 -> {
+            +boss[ActType.Act1]
+            +boss[ActType.Act4]
+            +boss[ActType.Act5]
+        }
+        3 -> {
+            +boss[ActType.ClimaxAct]
+            +boss[ActType.Act6]
+            +boss[ActType.Act4]
+        }
+        4 ->{
+            +boss[ActType.Act7]
+            +boss[ActType.Act2]
+            +boss[ActType.Act5]
+        }
+        5 ->{
+            +boss[ActType.Act8]
+            +boss[ActType.Act6]
+            +boss[ActType.Act4]
+        }
+        6 ->{
+            +boss[ActType.Act2]
+            +boss[ActType.Act4]
+            +boss[ActType.Act5]
+        }
+        else -> error("Not supported.")
+    }
+}
 
 val tr13SetsunaIchieDiff4 = ActorLoadout(
     "TR13 Setsuna Ichie Difficulty 4",
@@ -122,7 +275,7 @@ val tr13SetsunaIchieDiff4 = ActorLoadout(
             },
         ),
         autoSkills = listOf(
-            trEventBonusPassive(HopeLalafin),
+            trEventBonusPassive(JudgementShizuha),
             AbnormalGuardPassive.new(),
         ),
     ),
@@ -131,7 +284,7 @@ val tr13SetsunaIchieDiff4 = ActorLoadout(
 val tr13SetsunaIchieDiff4Strategy = FixedStrategy {
     val boss = this.team.actors.values.first()
 
-    when (turn) { //TODO() Actual moveset
+    when (turn) {
         1 -> {
             +boss[ActType.Act6]
             +boss[ActType.Act1]
@@ -139,22 +292,28 @@ val tr13SetsunaIchieDiff4Strategy = FixedStrategy {
         }
         2 -> {
             +boss[ActType.Act1]
-            +boss[ActType.Act3]
+            +boss[ActType.Act4]
             +boss[ActType.Act5]
         }
         3 -> {
             +boss[ActType.ClimaxAct]
             +boss[ActType.Act6]
-            +boss[ActType.Act3]
+            +boss[ActType.Act4]
         }
         4 ->{
-
+            +boss[ActType.Act7]
+            +boss[ActType.Act2]
+            +boss[ActType.Act5]
         }
         5 ->{
-
+            +boss[ActType.Act8]
+            +boss[ActType.Act6]
+            +boss[ActType.Act4]
         }
         6 ->{
-
+            +boss[ActType.Act2]
+            +boss[ActType.Act4]
+            +boss[ActType.Act5]
         }
         else -> error("Not supported.")
     }
