43cf45106493ee4a1985e742a13104645f1cfb74
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2022-01-14 22:44:10 -0800

    Update generated files

---
+++ relive-simulator-core/src/commonMain/kotlin/xyz/qwewqa/relive/simulator/core/presets/memoir/generated/equip4000224.kt
@@ -0,0 +1,54 @@
+package xyz.qwewqa.relive.simulator.core.presets.memoir.generated
+
+import xyz.qwewqa.relive.simulator.core.stage.actor.StatData
+import xyz.qwewqa.relive.simulator.core.stage.autoskill.EffectTag
+import xyz.qwewqa.relive.simulator.core.stage.dress.ActParameters
+import xyz.qwewqa.relive.simulator.core.stage.memoir.CutinBlueprint
+import xyz.qwewqa.relive.simulator.core.stage.memoir.PartialMemoirBlueprint
+
+val equip4000224 = PartialMemoirBlueprint(
+  id = 4000224,
+  name = "XXIII 希望【正位置】",
+  rarity = 4,
+  baseStats = StatData(
+    hp = 0,
+    actPower = 0,
+    normalDefense = 168,
+    specialDefense = 168,
+  ),
+  growthStats = StatData(
+    hp = 0,
+    actPower = 0,
+    normalDefense = 8000,
+    specialDefense = 8000,
+  ),
+  cutinData = CutinBlueprint(
+    costs = listOf(4, 4, 4, 4, 3),
+    startCooldowns = listOf(3, 3, 3, 3, 2),
+    cooldowns = listOf(3, 3, 3, 3, 2),
+    usageLimits = listOf(1, 1, 1, 1, 2),
+    parameters = listOf(
+      ActParameters(
+        values = listOf(0, 0, 0, 0, 0),
+        times = listOf(1, 1, 1, 1, 2),
+      ),
+      ActParameters(
+        values = listOf(),
+        times = listOf(),
+      ),
+      ActParameters(
+        values = listOf(),
+        times = listOf(),
+      ),
+      ActParameters(
+        values = listOf(),
+        times = listOf(),
+      ),
+      ActParameters(
+        values = listOf(),
+        times = listOf(),
+      ),
+    ),
+  ),
+  additionalTags = listOf(EffectTag.Lalafin)
+)
