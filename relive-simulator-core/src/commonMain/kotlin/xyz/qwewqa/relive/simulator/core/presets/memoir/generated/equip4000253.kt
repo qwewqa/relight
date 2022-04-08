ac97d6d13a6b265ec1aa0098f8655fcb6c1b1d7c
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2022-04-08 15:04:32 -0700

    Update icons and generated files

---
+++ relive-simulator-core/src/commonMain/kotlin/xyz/qwewqa/relive/simulator/core/presets/memoir/generated/equip4000253.kt
@@ -0,0 +1,54 @@
+package xyz.qwewqa.relive.simulator.core.presets.memoir.generated
+
+import xyz.qwewqa.relive.simulator.core.stage.actor.StatData
+import xyz.qwewqa.relive.simulator.core.stage.autoskill.EffectTag
+import xyz.qwewqa.relive.simulator.core.stage.dress.ActParameters
+import xyz.qwewqa.relive.simulator.core.stage.memoir.CutinBlueprint
+import xyz.qwewqa.relive.simulator.core.stage.memoir.PartialMemoirBlueprint
+
+val equip4000253 = PartialMemoirBlueprint(
+  id = 4000253,
+  name = "キラめきBirthday 野々宮ララフィン",
+  rarity = 4,
+  baseStats = StatData(
+    hp = 280,
+    actPower = 163,
+    normalDefense = 168,
+    specialDefense = 168,
+  ),
+  growthStats = StatData(
+    hp = 60000,
+    actPower = 3000,
+    normalDefense = 8000,
+    specialDefense = 8000,
+  ),
+  cutinData = CutinBlueprint(
+    costs = listOf(4, 4, 4, 4, 3),
+    startCooldowns = listOf(3, 3, 3, 3, 2),
+    cooldowns = listOf(3, 3, 3, 3, 2),
+    usageLimits = listOf(1, 1, 1, 1, 2),
+    parameters = listOf(
+      ActParameters(
+        values = listOf(10, 10, 12, 16, 20),
+        times = listOf(2, 2, 2, 2, 3),
+      ),
+      ActParameters(
+        values = listOf(10, 10, 12, 16, 20),
+        times = listOf(2, 2, 2, 2, 3),
+      ),
+      ActParameters(
+        values = listOf(10, 10, 12, 16, 20),
+        times = listOf(2, 2, 2, 2, 3),
+      ),
+      ActParameters(
+        values = listOf(10, 10, 12, 16, 20),
+        times = listOf(2, 2, 2, 2, 3),
+      ),
+      ActParameters(
+        values = listOf(),
+        times = listOf(),
+      ),
+    ),
+  ),
+  additionalTags = listOf(EffectTag.Lalafin)
+)
