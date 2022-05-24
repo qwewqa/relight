5b21d404c6862cf8b84baca8c3662a6c91625a56
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2022-05-23 17:28:59 -0700

    Update generated files

---
+++ relive-simulator-core/src/commonMain/kotlin/xyz/qwewqa/relive/simulator/core/presets/memoir/generated/equip4000268.kt
@@ -0,0 +1,54 @@
+package xyz.qwewqa.relive.simulator.core.presets.memoir.generated
+
+import xyz.qwewqa.relive.simulator.core.stage.actor.StatData
+import xyz.qwewqa.relive.simulator.core.stage.autoskill.EffectTag
+import xyz.qwewqa.relive.simulator.core.stage.dress.ActParameters
+import xyz.qwewqa.relive.simulator.core.stage.memoir.CutinBlueprint
+import xyz.qwewqa.relive.simulator.core.stage.memoir.PartialMemoirBlueprint
+
+val equip4000268 = PartialMemoirBlueprint(
+  id = 4000268,
+  name = "『BEACH！！』",
+  rarity = 4,
+  baseStats = StatData(
+    hp = 0,
+    actPower = 0,
+    normalDefense = 168,
+    specialDefense = 168,
+  ),
+  growthStats = StatData(
+    hp = 0,
+    actPower = 0,
+    normalDefense = 8000,
+    specialDefense = 8000,
+  ),
+  cutinData = CutinBlueprint(
+    costs = listOf(4, 4, 4, 4, 3),
+    startCooldowns = listOf(3, 3, 3, 3, 2),
+    cooldowns = listOf(3, 3, 3, 3, 2),
+    usageLimits = listOf(1, 1, 1, 1, 2),
+    parameters = listOf(
+      ActParameters(
+        values = listOf(10, 10, 15, 20, 25),
+        times = listOf(1, 1, 1, 1, 2),
+      ),
+      ActParameters(
+        values = listOf(10, 10, 15, 20, 25),
+        times = listOf(1, 1, 1, 1, 2),
+      ),
+      ActParameters(
+        values = listOf(),
+        times = listOf(),
+      ),
+      ActParameters(
+        values = listOf(),
+        times = listOf(),
+      ),
+      ActParameters(
+        values = listOf(),
+        times = listOf(),
+      ),
+    ),
+  ),
+  additionalTags = listOf(EffectTag.Akira, EffectTag.Yachiyo)
+)
---
+++ relive-simulator-core/src/commonMain/kotlin/xyz/qwewqa/relive/simulator/core/presets/memoir/generated/equip4000272.kt
@@ -0,0 +1,54 @@
+package xyz.qwewqa.relive.simulator.core.presets.memoir.generated
+
+import xyz.qwewqa.relive.simulator.core.stage.actor.StatData
+import xyz.qwewqa.relive.simulator.core.stage.autoskill.EffectTag
+import xyz.qwewqa.relive.simulator.core.stage.dress.ActParameters
+import xyz.qwewqa.relive.simulator.core.stage.memoir.CutinBlueprint
+import xyz.qwewqa.relive.simulator.core.stage.memoir.PartialMemoirBlueprint
+
+val equip4000272 = PartialMemoirBlueprint(
+  id = 4000272,
+  name = "目指すはポジションゼロ 晶",
+  rarity = 4,
+  baseStats = StatData(
+    hp = 445,
+    actPower = 113,
+    normalDefense = 363,
+    specialDefense = 363,
+  ),
+  growthStats = StatData(
+    hp = 45000,
+    actPower = 3000,
+    normalDefense = 3000,
+    specialDefense = 3000,
+  ),
+  cutinData = CutinBlueprint(
+    costs = listOf(5, 5, 5, 5, 4),
+    startCooldowns = listOf(3, 3, 3, 3, 2),
+    cooldowns = listOf(3, 3, 3, 3, 2),
+    usageLimits = listOf(1, 1, 1, 1, 2),
+    parameters = listOf(
+      ActParameters(
+        values = listOf(1, 1, 1, 1, 2),
+        times = listOf(0, 0, 0, 0, 0),
+      ),
+      ActParameters(
+        values = listOf(30, 30, 35, 40, 50),
+        times = listOf(1, 1, 1, 1, 2),
+      ),
+      ActParameters(
+        values = listOf(),
+        times = listOf(),
+      ),
+      ActParameters(
+        values = listOf(),
+        times = listOf(),
+      ),
+      ActParameters(
+        values = listOf(),
+        times = listOf(),
+      ),
+    ),
+  ),
+  additionalTags = listOf(EffectTag.Akira)
+)
