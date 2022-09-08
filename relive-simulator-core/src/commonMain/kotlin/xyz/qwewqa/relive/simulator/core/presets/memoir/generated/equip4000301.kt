7ddfd5dd1d66dfa642443fbf898986409608baac
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2022-09-08 00:19:55 -0700

    Update generated files

---
+++ relive-simulator-core/src/commonMain/kotlin/xyz/qwewqa/relive/simulator/core/presets/memoir/generated/equip4000301.kt
@@ -0,0 +1,59 @@
+package xyz.qwewqa.relive.simulator.core.presets.memoir.generated
+
+import xyz.qwewqa.relive.simulator.core.stage.actor.StatData
+import xyz.qwewqa.relive.simulator.core.stage.autoskill.EffectTag
+import xyz.qwewqa.relive.simulator.core.stage.dress.ActParameters
+import xyz.qwewqa.relive.simulator.core.stage.memoir.CutinBlueprint
+import xyz.qwewqa.relive.simulator.core.stage.memoir.PartialMemoirBlueprint
+
+val equip4000301 = PartialMemoirBlueprint(
+  id = 4000301,
+  name = "私の誇り",
+  rarity = 4,
+  baseStats = StatData(
+    hp = 0,
+    actPower = 0,
+    normalDefense = 168,
+    specialDefense = 168,
+  ),
+  growthStats = StatData(
+    hp = 0,
+    actPower = 0,
+    normalDefense = 8000,
+    specialDefense = 8000,
+  ),
+  cutinData = CutinBlueprint(
+    costs = listOf(4, 4, 4, 4, 3),
+    startCooldowns = listOf(3, 3, 3, 3, 2),
+    cooldowns = listOf(3, 3, 3, 3, 2),
+    usageLimits = listOf(1, 1, 1, 1, 2),
+    parameters = listOf(
+      ActParameters(
+        values = listOf(20000, 20000, 30000, 35000, 40000),
+        times = listOf(0, 0, 0, 0, 0),
+        hitRate = 100,
+      ),
+      ActParameters(
+        values = listOf(),
+        times = listOf(),
+        hitRate = 0,
+      ),
+      ActParameters(
+        values = listOf(),
+        times = listOf(),
+        hitRate = 0,
+      ),
+      ActParameters(
+        values = listOf(),
+        times = listOf(),
+        hitRate = 0,
+      ),
+      ActParameters(
+        values = listOf(),
+        times = listOf(),
+        hitRate = 0,
+      ),
+    ),
+  ),
+  additionalTags = listOf(EffectTag.Fumi, EffectTag.Akira)
+)
