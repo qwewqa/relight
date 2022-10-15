aa4e9c089d7d59f3747920a7aacddcf7e5d202b6
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2022-10-15 16:44:33 -0700

    Remove codemirror.css from precache

--- relive-simulator-browser/build.gradle.kts
+++ relive-simulator-browser/build.gradle.kts
@@ -101,7 +101,6 @@ tasks.register<Copy>("copyServiceWorker") {
 {url: 'relive-simulator-worker.js', revision: '$timestamp'},
 {url: 'bootstrap-select.js', revision: '$timestamp'},
 {url: 'options.json', revision: '$timestamp'},
-{url: 'codemirror.css', revision: '$timestamp'},
 {url: 'style.css', revision: '$timestamp'},
 ${imageData.joinToString(",\n") { (url, revision) -> "{url: '$url', revision: '$revision'}" }}
             """.trimIndent()
