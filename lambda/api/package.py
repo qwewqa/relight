857938c0c414f0ae532a5331f020b8dda436aa76
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2023-05-21 18:58:09 -0400

    Move to using a zip for lambda

---
+++ lambda/api/package.py
@@ -0,0 +1,41 @@
+import zipfile
+from pathlib import Path
+
+# Run `pip install --platform=manylinux1_x86_64 --only-binary=:all: --python-version 3.9 -r requirements.txt -t ./deps`
+DEPENDENCIES = "deps"
+
+EXCLUDED_PACKAGES = [
+    "boto3",
+    "botocore",
+    "pip",
+    "setuptools",
+]
+
+APP_PATHS = [
+    "utils",
+    "lambda_function.py",
+]
+
+
+def write_to_zip(zip_file: zipfile.ZipFile, base: Path, path: Path = None):
+    if path is None:
+        path = base
+    if path.is_dir():
+        for child in path.iterdir():
+            write_to_zip(zip_file, base, child)
+    else:
+        zip_file.write(path, path.relative_to(base))
+
+
+def package_for_lambda():
+    with zipfile.ZipFile("lambda.zip", "w") as zip_file:
+        for path in APP_PATHS:
+            write_to_zip(zip_file, Path(__file__).parent, Path(__file__).parent / Path(path))
+        for path in Path(DEPENDENCIES).iterdir():
+            if any(path.name.startswith(excluded) for excluded in EXCLUDED_PACKAGES):
+                continue
+            write_to_zip(zip_file, Path(DEPENDENCIES), path)
+
+
+if __name__ == "__main__":
+    package_for_lambda()
