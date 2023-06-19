3993d05307b9712bb24742110e64ebcd81269ce2
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2023-06-19 19:00:29 -0400

    Update flag layout

--- relive-simulator-client/src/main/kotlin/xyz/qwewqa/relive/simulator/client/FlagData.kt
+++ relive-simulator-client/src/main/kotlin/xyz/qwewqa/relive/simulator/client/FlagData.kt
@@ -3,12 +3,13 @@ package xyz.qwewqa.relive.simulator.client
 import kotlinx.browser.document
 import kotlinx.dom.clear
 import kotlinx.html.dom.append
-import kotlinx.html.h4
+import kotlinx.html.figcaption
+import kotlinx.html.i
 import kotlinx.html.img
 import kotlinx.html.js.a
-import kotlinx.html.js.div
-import kotlinx.html.js.i
+import kotlinx.html.js.figure
 import kotlinx.html.js.small
+import kotlinx.html.style
 import org.w3c.dom.HTMLDivElement
 import kotlin.random.Random
 
@@ -23,13 +24,69 @@ data class ArtistInfo(
     val website: String? = null,
 )
 
+enum class FlagCharacterGroup(val color: String) {
+  Misc("#888888"),
+  Seisho("#f7545d"),
+  Rinmeikan("#ba65eb"),
+  Frontier("#ffc134"),
+  Siegfeld("#6880f3"),
+  Seiran("#1f5ba5"),
+//  Misc("#eeeeee"),
+//  Seisho("#f6c5c8"),
+//  Rinmeikan("#f8e4fe"),
+//  Frontier("#faebcb"),
+//  Siegfeld("#d4dcf0"),
+//  Seiran("#cee3f2"),
+}
+
+enum class FlagCharacter(val group: FlagCharacterGroup) {
+  Kirin(FlagCharacterGroup.Misc),
+  Andrew(FlagCharacterGroup.Misc),
+  Ellen(FlagCharacterGroup.Misc),
+  Amemiya(FlagCharacterGroup.Misc),
+  Masai(FlagCharacterGroup.Misc),
+  Karen(FlagCharacterGroup.Seisho),
+  Hikari(FlagCharacterGroup.Seisho),
+  Mahiru(FlagCharacterGroup.Seisho),
+  Claudine(FlagCharacterGroup.Seisho),
+  Maya(FlagCharacterGroup.Seisho),
+  Junna(FlagCharacterGroup.Seisho),
+  Nana(FlagCharacterGroup.Seisho),
+  Futaba(FlagCharacterGroup.Seisho),
+  Kaoruko(FlagCharacterGroup.Seisho),
+  Tamao(FlagCharacterGroup.Rinmeikan),
+  Ichie(FlagCharacterGroup.Rinmeikan),
+  Fumi(FlagCharacterGroup.Rinmeikan),
+  Rui(FlagCharacterGroup.Rinmeikan),
+  Yuyuko(FlagCharacterGroup.Rinmeikan),
+  Aruru(FlagCharacterGroup.Frontier),
+  Misora(FlagCharacterGroup.Frontier),
+  Lalafin(FlagCharacterGroup.Frontier),
+  Tsukasa(FlagCharacterGroup.Frontier),
+  Shizuha(FlagCharacterGroup.Frontier),
+  Akira(FlagCharacterGroup.Siegfeld),
+  Michiru(FlagCharacterGroup.Siegfeld),
+  MeiFan(FlagCharacterGroup.Siegfeld),
+  Shiori(FlagCharacterGroup.Siegfeld),
+  Yachiyo(FlagCharacterGroup.Siegfeld),
+  Stella(FlagCharacterGroup.Siegfeld),
+  Shiro(FlagCharacterGroup.Siegfeld),
+  Ryoko(FlagCharacterGroup.Siegfeld),
+  Minku(FlagCharacterGroup.Siegfeld),
+  Kuina(FlagCharacterGroup.Siegfeld),
+  Koharu(FlagCharacterGroup.Seiran),
+  Suzu(FlagCharacterGroup.Seiran),
+  Hisame(FlagCharacterGroup.Seiran),
+}
+
 data class FlagInfo(
     val artist: ArtistInfo,
+    val character: FlagCharacter,
     val name: String,
     val weight: Double = 1.0,
 ) {
-  val smallUrl = "img/flags/${artist.name.lowercase()}_${name}_sm.png"
-  val largeUrl = "img/flags/${artist.name.lowercase()}_${name}.png"
+  val smallUrl = "img/flags/${artist.name.lowercase()}_${character.name.lowercase()}_${name}_sm.png"
+  val largeUrl = "img/flags/${artist.name.lowercase()}_${character.name.lowercase()}_${name}.png"
 }
 
 object FlagData {
@@ -79,58 +136,74 @@ object FlagData {
           twitterId = "1515186747834978304",
       )
 
-  val anonymousArtemisAkiraAro =
+  val limeMahiruCupidLesbian =
       +FlagInfo(
-          artist = anonymous,
-          name = "artemis_akira_aro",
+          artist = lime,
+          character = FlagCharacter.Mahiru,
+          name = "cupid_lesbian",
       )
 
   val anonymousValentinesJunnaLesbian =
       +FlagInfo(
           artist = anonymous,
-          name = "valentines_junna_lesbian",
-      )
-
-  val cyMeiFanHikoboshiPan =
-      +FlagInfo(
-          artist = cy,
-          name = "meifan_hikoboshi_pan",
-      )
-
-  val cyTsukasaBenzaitenLesbian =
-      +FlagInfo(
-          artist = cy,
-          name = "tsukasa_benzaiten_lesbian",
+          character = FlagCharacter.Junna,
+          name = "valentines_lesbian",
       )
 
   val huiyuanNanaFruitsTartPride =
       +FlagInfo(
           artist = huiyuan,
-          name = "nana_fruits_tart_pride",
-      )
-
-  val limeKuinaFsgAgenderAromantic =
-      +FlagInfo(
-          artist = lime,
-          name = "kuina_fsg_agender_aro",
+          character = FlagCharacter.Nana,
+          name = "fruits_tart_pride",
       )
 
   val kekkersRuiBeachTrans =
       +FlagInfo(
           artist = kekkers,
-          name = "rui_beach_trans",
+          character = FlagCharacter.Rui,
+          name = "beach_trans",
       )
 
-  val vaniRyokoCasualAgender =
+  val cyTsukasaBenzaitenLesbian =
       +FlagInfo(
-          artist = vani,
-          name = "ryoko_casual_agender",
+          artist = cy,
+          character = FlagCharacter.Tsukasa,
+          name = "benzaiten_lesbian",
+      )
+
+  val anonymousArtemisAkiraAro =
+      +FlagInfo(
+          artist = anonymous,
+          character = FlagCharacter.Akira,
+          name = "artemis_aro",
+      )
+
+  val cyMeiFanHikoboshiPan =
+      +FlagInfo(
+          artist = cy,
+          character = FlagCharacter.MeiFan,
+          name = "hikoboshi_pan",
       )
 
   val vaniShioriShutenBi =
       +FlagInfo(
           artist = vani,
-          name = "shiori_shuten_bi",
+          character = FlagCharacter.Shiori,
+          name = "shuten_bi",
+      )
+
+  val vaniRyokoCasualAgender =
+      +FlagInfo(
+          artist = vani,
+          character = FlagCharacter.Ryoko,
+          name = "casual_agender",
+      )
+
+  val limeKuinaFsgAgenderAromantic =
+      +FlagInfo(
+          artist = lime,
+          character = FlagCharacter.Kuina,
+          name = "fsg_agender_aro",
       )
 }
 
@@ -158,14 +231,14 @@ fun initAboutArtists() {
 
   if (ele.childElementCount > 0) return
 
-  val flagsByArtist = FlagData.flags.groupBy { it.artist }
-
   ele.clear()
   ele.append {
-    for (artist in FlagData.artists) {
-      val flags = flagsByArtist[artist] ?: continue
-      div("my-3") {
-        h4 {
+    FlagData.flags.forEach { flag ->
+      figure("art-list-figure") {
+        style = "border: 2px solid ${flag.character.group.color};"
+        img(classes = "art-list-img", src = flag.largeUrl)
+        figcaption {
+          val artist = flag.artist
           +artist.name
           +" "
           small {
@@ -191,13 +264,6 @@ fun initAboutArtists() {
                 target = "_blank"
                 i("bi bi-pixiv")
               }
-            }
-          }
-        }
-        div("d-flex flex-wrap") {
-          for (flag in flags) {
-            img(classes = "art-list-img") {
-              src = flag.largeUrl
             }
           }
         }
