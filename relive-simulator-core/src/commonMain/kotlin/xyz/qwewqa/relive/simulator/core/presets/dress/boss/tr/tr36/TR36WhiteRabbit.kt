373dbd38239b317a0f1c95edd5df5b0beb242cfe
Savivo <n.savivo@gmail.com>
2023-11-15 22:31:15 -0500

    TR36 Bosses

---
+++ relive-simulator-core/src/commonMain/kotlin/xyz/qwewqa/relive/simulator/core/presets/dress/boss/tr/tr36/TR36WhiteRabbit.kt
@@ -0,0 +1,155 @@
+package xyz.qwewqa.relive.simulator.core.presets.dress.boss.tr.tr36
+
+import xyz.qwewqa.relive.simulator.core.presets.dress.boss.tr.FullNegativeEffectResistancePassive
+import xyz.qwewqa.relive.simulator.core.presets.dress.boss.tr.TrDamageReductionPassive
+import xyz.qwewqa.relive.simulator.core.presets.dress.boss.tr.trEventBonusPassive2023V2
+import xyz.qwewqa.relive.simulator.core.stage.HitMode
+import xyz.qwewqa.relive.simulator.core.stage.actor.ActType
+import xyz.qwewqa.relive.simulator.core.stage.actor.Attribute
+import xyz.qwewqa.relive.simulator.core.stage.actor.Character
+import xyz.qwewqa.relive.simulator.core.stage.actor.DamageType
+import xyz.qwewqa.relive.simulator.core.stage.actor.Position
+import xyz.qwewqa.relive.simulator.core.stage.actor.actsOf
+import xyz.qwewqa.relive.simulator.core.stage.actor.defaultDressStats
+import xyz.qwewqa.relive.simulator.core.stage.autoskill.AbnormalGuardPassive
+import xyz.qwewqa.relive.simulator.core.stage.autoskill.BossElementResistPassive
+import xyz.qwewqa.relive.simulator.core.stage.autoskill.new
+import xyz.qwewqa.relive.simulator.core.stage.buff.BuffCategory
+import xyz.qwewqa.relive.simulator.core.stage.buff.Buffs
+import xyz.qwewqa.relive.simulator.core.stage.dress.Dress
+import xyz.qwewqa.relive.simulator.core.stage.loadout.ActorLoadout
+import xyz.qwewqa.relive.simulator.core.stage.strategy.FixedStrategy
+
+val tr36WhiteRabbitMisora =
+    ActorLoadout(
+        "TR36 White Rabbit Misora",
+        Dress(
+            name = "White Rabbit Misora",
+            character = Character.Misora,
+            attribute = Attribute.Dream,
+            damageType = DamageType.Special,
+            position = Position.None,
+            stats =
+            defaultDressStats.copy(
+                hp = 90_000_000,
+                actPower = 2300,
+                normalDefense = 650,
+                specialDefense = 650,
+                agility = 9999,
+            ),
+            acts =
+            actsOf(
+            ),
+            autoSkills =
+            listOf(
+                trEventBonusPassive2023V2(2050013),
+                AbnormalGuardPassive.new(),
+            ),
+        ),
+    )
+
+val tr36WhiteRabbitMisoraStrategy = FixedStrategy {
+  val boss = this.team.actors.values.first()
+
+  when (turn) {
+    1 -> {
+      +boss[ActType.Act6]
+      +boss[ActType.Act1]
+      +boss[ActType.Act3]
+    }
+    2 -> {
+      +boss[ActType.Act1]
+      +boss[ActType.Act4]
+      +boss[ActType.Act5]
+    }
+    3 -> {
+      +boss[ActType.ClimaxAct]
+      +boss[ActType.Act6]
+      +boss[ActType.Act4]
+    }
+    4 -> {
+      +boss[ActType.Act7]
+      +boss[ActType.Act2]
+      +boss[ActType.Act5]
+    }
+    5 -> {
+      +boss[ActType.Act8]
+      +boss[ActType.Act6]
+      +boss[ActType.Act4]
+    }
+    6 -> {
+      +boss[ActType.Act2]
+      +boss[ActType.Act4]
+      +boss[ActType.Act5]
+    }
+    else -> error("Not supported.")
+  }
+}
+
+val tr36WhiteRabbitMisoraDiff4 =
+    ActorLoadout(
+        "TR36 White Rabbit Misora Difficulty 4",
+        Dress(
+            name = "White Rabbit Misora",
+            character = Character.Misora,
+            attribute = Attribute.Dream,
+            damageType = DamageType.Special,
+            position = Position.None,
+            stats =
+            defaultDressStats.copy(
+                hp = 1_200_000_000,
+                actPower = 10000,
+                normalDefense = 10000,
+                specialDefense = 10000,
+                agility = 9999,
+            ),
+            acts =
+            actsOf(
+            ),
+            autoSkills =
+            listOf(
+                trEventBonusPassive2023V2(2050013),
+                FullNegativeEffectResistancePassive.new(100),
+                BossElementResistPassive.new(50),
+                TrDamageReductionPassive.new(90),
+            ),
+        ),
+    )
+
+val tr36WhiteRabbitMisoraDiff4Strategy = FixedStrategy {
+  val boss = this.team.actors.values.first()
+
+  when (turn) {
+    1 -> {
+      +boss[ActType.Act6]
+      +boss[ActType.Act1]
+      +boss[ActType.Act3]
+    }
+    2 -> {
+      +boss[ActType.Act1]
+      +boss[ActType.Act4]
+      +boss[ActType.Act5]
+    }
+    3 -> {
+      +boss[ActType.ClimaxAct]
+      +boss[ActType.Act6]
+      +boss[ActType.Act4]
+    }
+    4 -> {
+      +boss[ActType.Act7]
+      +boss[ActType.Act2]
+      +boss[ActType.Act5]
+    }
+    5 -> {
+      +boss[ActType.Act8]
+      +boss[ActType.Act6]
+      +boss[ActType.Act4]
+    }
+    6 -> {
+      +boss[ActType.Act2]
+      +boss[ActType.Act4]
+      +boss[ActType.Act5]
+    }
+    else -> error("Not supported.")
+  }
+}
