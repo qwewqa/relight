1434dadf22d6260b931e9b8698fbb6eff2a6ab15
Savivo <n.savivo@gmail.com>
2023-12-07 17:36:23 -0500

    TR37 Bosses

---
+++ relive-simulator-core/src/commonMain/kotlin/xyz/qwewqa/relive/simulator/core/presets/dress/boss/tr/tr37/TR37SuirenYuyuko.kt
@@ -0,0 +1,163 @@
+package xyz.qwewqa.relive.simulator.core.presets.dress.boss.tr.tr37
+
+import xyz.qwewqa.relive.simulator.core.presets.dress.boss.tr.FullNegativeEffectResistancePassive
+import xyz.qwewqa.relive.simulator.core.presets.dress.boss.tr.SuperBossPassive2
+import xyz.qwewqa.relive.simulator.core.presets.dress.boss.tr.TrDamageReductionPassive
+import xyz.qwewqa.relive.simulator.core.presets.dress.boss.tr.trEventBonusPassive2023V2
+import xyz.qwewqa.relive.simulator.core.stage.HitMode
+import xyz.qwewqa.relive.simulator.core.stage.actor.ActType
+import xyz.qwewqa.relive.simulator.core.stage.actor.Attribute
+import xyz.qwewqa.relive.simulator.core.stage.actor.Character
+import xyz.qwewqa.relive.simulator.core.stage.actor.DamageType
+import xyz.qwewqa.relive.simulator.core.stage.actor.Position
+import xyz.qwewqa.relive.simulator.core.stage.actor.actsOf
+import xyz.qwewqa.relive.simulator.core.stage.actor.defaultDressStats
+import xyz.qwewqa.relive.simulator.core.stage.autoskill.AbnormalResistPassiveA
+import xyz.qwewqa.relive.simulator.core.stage.autoskill.BossElementResistPassive
+import xyz.qwewqa.relive.simulator.core.stage.autoskill.new
+import xyz.qwewqa.relive.simulator.core.stage.buff.Buffs
+import xyz.qwewqa.relive.simulator.core.stage.dress.Dress
+import xyz.qwewqa.relive.simulator.core.stage.loadout.ActorLoadout
+import xyz.qwewqa.relive.simulator.core.stage.strategy.FixedStrategy
+
+val tr37SuirenYuyuko =
+    ActorLoadout(
+        "TR37 Suiren Yuyuko",
+        Dress(
+            name = "Suiren Yuyuko",
+            character = Character.Yuyuko,
+            attribute = Attribute.Snow,
+            damageType = DamageType.Special,
+            position = Position.None,
+            stats =
+            defaultDressStats.copy(
+                hp = 375_000_000,
+                actPower = 2100,
+                normalDefense = 650,
+                specialDefense = 650,
+                agility = 9999,
+            ),
+            acts =
+            actsOf(
+            ),
+            autoSkills =
+            listOf(
+                trEventBonusPassive2023V2(3010023),
+                SuperBossPassive2.new(100),
+                BossElementResistPassive.new(50),
+                TrDamageReductionPassive.new(80),
+                //TRCritDamageReductionPassive.new(?)
+            ),
+        ),
+    )
+
+val tr37SuirenYuyukoStrategy = FixedStrategy {
+  val boss = this.team.actors.values.first()
+
+  when (turn) {
+    1 -> {
+      +boss[ActType.Act1]
+      +boss[ActType.Act2]
+      +boss[ActType.Act6]
+    }
+    2 -> {
+      +boss[ActType.Act1]
+      +boss[ActType.Act3]
+      +boss[ActType.Act6]
+    }
+    3 -> {
+      +boss[ActType.ClimaxAct]
+      +boss[ActType.Act5]
+      +boss[ActType.Act4]
+    }
+    4 -> {
+      +boss[ActType.Act6]
+      +boss[ActType.Act1]
+      +boss[ActType.Act3]
+    }
+    5 -> {
+      +boss[ActType.Act7]
+      +boss[ActType.Act6]
+      +boss[ActType.Act4]
+    }
+    6 -> {
+      +boss[ActType.Act6]
+      +boss[ActType.Act5]
+      +boss[ActType.Act4]
+    }
+    else -> {
+      error("Unsupported.")
+    }
+  }
+}
+
+val tr37SuirenYuyukoDiff4 =
+    ActorLoadout(
+        "TR37 Suiren Yuyuko Difficulty 4",
+        Dress(
+            name = "Suiren Yuyuko",
+            character = Character.Yuyuko,
+            attribute = Attribute.Snow,
+            damageType = DamageType.Special,
+            position = Position.None,
+            stats =
+            defaultDressStats.copy(
+                hp = 1_500_000_000,
+                actPower = 10000,
+                normalDefense = 10000,
+                specialDefense = 10000,
+                agility = 9999,
+            ),
+            acts =
+            actsOf(
+            ),
+            autoSkills =
+            listOf(
+                trEventBonusPassive2023V2(3010023),
+                SuperBossPassive2.new(100),
+                BossElementResistPassive.new(50),
+                TrDamageReductionPassive.new(99),
+                //TRCritDamageReductionPassive.new(?)
+            ),
+        ),
+    )
+
+val tr37SuirenYuyukoDiff4Strategy = FixedStrategy {
+  val boss = this.team.actors.values.first()
+
+  when (turn) {
+    1 -> {
+      +boss[ActType.Act1]
+      +boss[ActType.Act2]
+      +boss[ActType.Act6]
+    }
+    2 -> {
+      +boss[ActType.Act1]
+      +boss[ActType.Act3]
+      +boss[ActType.Act6]
+    }
+    3 -> {
+      +boss[ActType.ClimaxAct]
+      +boss[ActType.Act5]
+      +boss[ActType.Act4]
+    }
+    4 -> {
+      +boss[ActType.Act6]
+      +boss[ActType.Act1]
+      +boss[ActType.Act3]
+    }
+    5 -> {
+      +boss[ActType.Act7]
+      +boss[ActType.Act6]
+      +boss[ActType.Act4]
+    }
+    6 -> {
+      +boss[ActType.Act6]
+      +boss[ActType.Act5]
+      +boss[ActType.Act4]
+    }
+    else -> {
+      error("Unsupported.")
+    }
+  }
+}
